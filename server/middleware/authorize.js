a7f65f5a094bfd44721a2b0a1634fc96247abd7c
joelkassa <joelkassa159@gmail.com>
2026-07-07 04:30:00 -0600

    feat: add forgot password page and error handling middleware

---
+++ server/middleware/authorize.js
@@ -0,0 +1,15 @@
+function authorize(...allowedRoles) {
+  return (req, res, next) => {
+    if (!req.user || !allowedRoles.includes(req.user.role)) {
+      return res.status(403).json({ message: 'You do not have permission to do this.' });
+    }
+    next();
+  };
+}
+
+module.exports = { authorize };
+
+
+
+
+
